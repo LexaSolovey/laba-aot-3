ac8ba9445929f8bbeabf83e9afb5f466279a209e
alekseyAndrosuk <aleksey.androsuk@orangesoft.by>
2019-01-09 01:02:24 +0300

    Updates

---
+++ precache-manifest.c3f6c9c05ccbba7d1696fcbbcb5dc9a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/laba-aot-3/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b341bbcef70f2cc5480c",
+    "url": "/laba-aot-3/static/js/runtime~main.b341bbce.js"
+  },
+  {
+    "revision": "8cf6edf767133452e96f",
+    "url": "/laba-aot-3/static/js/main.8cf6edf7.chunk.js"
+  },
+  {
+    "revision": "14dd8d698f8c7de0c529",
+    "url": "/laba-aot-3/static/js/1.14dd8d69.chunk.js"
+  },
+  {
+    "revision": "8cf6edf767133452e96f",
+    "url": "/laba-aot-3/static/css/main.06912725.chunk.css"
+  },
+  {
+    "revision": "a560f0f30a2d11125a99654c26daf6bb",
+    "url": "/laba-aot-3/index.html"
+  }
+];
